3674118d0cb5712d371b1ae98664eb80815cb40e
Alexander <alexander.v.tarasyuk@gmail.com>
2018-09-30 13:58:21 +0300

    fix formatting

--- template/javascript/babel.config.js
+++ template/javascript/babel.config.js
@@ -1,17 +1,17 @@
 module.exports = function(api) {
-	api.cache.forever();
+  api.cache.forever();
 
-	if (process.env.RX_PLATFORM === 'web') {
-	  return {
-		presets: [
-		  ['@babel/preset-env', { 'targets': { 'browsers': ['last 2 versions'] } }],
-		  ['@babel/preset-react', {}],
-		],
-		plugins: ['@babel/plugin-transform-runtime'],
-	  };
-	}
+  if (process.env.RX_PLATFORM === 'web') {
+    return {
+    presets: [
+      ['@babel/preset-env', { 'targets': { 'browsers': ['last 2 versions'] } }],
+      ['@babel/preset-react', {}],
+    ],
+    plugins: ['@babel/plugin-transform-runtime'],
+    };
+  }
 
-	return {
-	  presets: ['module:metro-react-native-babel-preset'],
-	};
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+  };
 };
