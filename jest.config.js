4bbf899fa0ad378c3bd25b53f2e1a2fa20287971
Alexander T <alexander.tarasyuk@outlook.com>
2018-11-15 12:42:53 +0200

    add tests

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+
+  rootDir: '.',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+
+  moduleFileExtensions: [
+    'ts', 'tsx', 'js', 'json', 'node',
+  ],
+
+  moduleDirectories: ['node_modules'],
+};
